77ccc2939bf35677dc4a1fe1321616d0de67471c
nomorechokedboy <haiduongit@protonmail.com>
2021-09-18 15:10:40 +0700

    initialize again

---
+++ components/HomePage/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Header from "../shards/Header";
+
+export default function index() {
+  return <Header></Header>;
+}
---
+++ components/shards/Header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./header.module.css";
+
+export default function Header() {
+  return (
+    <div className={styles.test}>
+      <h1>Dcm tui java oc cho luon</h1>
+    </div>
+  );
+}
