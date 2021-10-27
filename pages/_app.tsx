accfc3c7bc8b75a842f3d92dea4ce4917fee61ee
nomorechokedboy <haiduongit@protonmail.com>
2021-10-27 16:54:37 +0700

    add todo search state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,15 @@ import "../styles/globals.css";
 import "../styles/reset.css";
 import "../styles/globals.css";
 import "../public/fontawesome-free-5.15.4-web/css/all.min.css";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
