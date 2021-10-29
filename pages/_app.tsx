36caa19dc9c989f87540ff5846d549fc245ed21c
nomorechokedboy <haiduongit@protonmail.com>
2021-10-29 15:02:18 +0700

    add todos state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,9 @@ import "../styles/globals.css";
 import "../public/fontawesome-free-5.15.4-web/css/all.min.css";
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
+import { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
