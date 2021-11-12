9509419c093ef5d443f7e056b5f469ca3499ab30
cpea2506 <vienpro2506@gmail.com>
2021-11-12 17:33:49 +0700

    use axios api instead of fetch api

--- pages/api/getUser.ts
+++ pages/api/getUser.ts
@@ -1,3 +1,5 @@
+import * as axios from "axios";
+
 const USER_API = "http://localhost:5001/api/v1/user";
 
 interface User {
@@ -10,42 +12,42 @@ interface SignupUser extends User {
   rePassword: string;
 }
 
-async function getResult(path: string, headers: HeadersInit, body: string) {
-  let response = await fetch(`${USER_API}${path}`, {
-    method: "POST",
-    headers,
-    body,
-  });
-
-  if (response.ok) {
-    const token = await response.text();
-    return { success: true, token };
-  }
-
-  const errors = await response.json();
-  return { success: false, errors };
+interface APIOptions {
+  path: string;
+  data: Object;
 }
 
-export const signupStatus = async (user: SignupUser) => {
-  const headers = {
-    "Content-Type": "application/json",
-  };
-  const body = JSON.stringify(user);
-  return getResult("/signup", headers, body);
-};
+interface APIResult {
+  success: boolean;
+  token?: string;
+  error?: string;
+}
 
-export const loginStatus = async (user: User) => {
-  const headers = {
-    "Content-Type": "application/json",
-  };
-  const body = JSON.stringify(user);
-  return getResult("/login", headers, body);
-};
+async function getResult({ path, data }: APIOptions): Promise<APIResult> {
+  try {
+    const response: axios.AxiosResponse = await axios.default.post(
+      `${USER_API}${path}`,
+      {
+        ...data,
+      }
+    );
 
-export const googleLoginStatus = async (token: string) => {
-  const headers = {
-    "Content-Type": "application/json",
-  };
-  const body = JSON.stringify({ token });
-  return getResult("/google/login", headers, body);
-};
+    const token = response.data;
+    return { success: true, token };
+  } catch (error) {
+    const message: string = Object.values(error.response.data)[0][0];
+    return { success: false, error: message };
+  }
+}
+
+export async function signupStatus(user: SignupUser): Promise<APIResult> {
+  return getResult({ path: "/signup", data: user });
+}
+
+export async function loginStatus(user: User): Promise<APIResult> {
+  return getResult({ path: "/login", data: user });
+}
+
+export async function googleLoginStatus(token: string): Promise<APIResult> {
+  return getResult({ path: "/google/login", data: { token } });
+}
