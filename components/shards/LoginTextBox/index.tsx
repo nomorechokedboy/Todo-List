97eb72342eea7525d7744901538e3c1f8472f4b3
cpea2506 <vienpro2506@gmail.com>
2021-11-16 02:51:55 +0700

    Add password showing

--- components/shards/LoginTextBox/index.tsx
+++ components/shards/LoginTextBox/index.tsx
@@ -1,42 +1,70 @@
+import { useMemo, useState } from "react";
 import styles from "./loginTextBox.module.css";
 
 interface ILoginTextBoxProps {
   id: string;
   type: string;
-  onChange?: React.ChangeEventHandler<HTMLInputElement>;
   placeholder: string;
   iconClass: string;
   label: string;
-  className: string;
   name: string;
+  pwdShow?: boolean;
+  pattern?: string;
+  title?: string;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  onInvalid?: React.FormEventHandler<HTMLInputElement>;
+  onInput?: React.FormEventHandler<HTMLInputElement>;
 }
 
 export default function LoginTextBox({
-  id,
-  onChange,
-  type,
   iconClass,
   label,
-  placeholder,
-  className,
-  name,
+  type,
+  id,
+  ...inputProps
 }: ILoginTextBoxProps) {
+  const [pwd, setPwd] = useState({
+    show: false,
+    type: "password",
+  });
+
+  const isPwd = type === "password";
+
+  const handlePwdShowClick = () => {
+    setPwd({
+      show: !pwd.show,
+      type: !pwd.show ? "text" : "password",
+    });
+  };
+
+  const icon = useMemo(() => {
+    return {
+      openEyes: "fas fa-eye-slash fa-lg",
+      closeEyes: "fas fa-eye fa-lg",
+    };
+  }, []);
+
   return (
     <>
-      <div className={className}>
+      <div className={styles.inputContainer}>
         <label htmlFor={id} className={styles.label}>
           {label}
         </label>
+
         <div className={styles.textBox}>
           <i className={iconClass}></i>
           <input
-            onChange={onChange}
-            id={id}
-            name={name}
             className={styles.input}
-            placeholder={placeholder}
-            type={type}
+            type={isPwd ? pwd.type : type}
+            {...inputProps}
+            required
           />
+
+          {isPwd && (
+            <div className={styles.pwdShow} onClick={handlePwdShowClick}>
+              <i className={pwd.show ? icon.closeEyes : icon.openEyes} />
+            </div>
+          )}
         </div>
       </div>
     </>
