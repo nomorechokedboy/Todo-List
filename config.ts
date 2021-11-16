e97de84c3177ce73f54823376e837110ff3d8038
cpea2506 <vienpro2506@gmail.com>
2021-11-16 22:19:32 +0700

    Expose GOOGLE_CLIENT_ID to browser

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
-export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+export const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
 export const NODE_ENV = process.env.NODE_ENV;
 export const PORT = process.env.PORT;
